1c82f1dad4636a9065dd37d4adc8efa13a1ae506
Investment-guy-Paper <96vyj7hzpt@privaterelay.appleid.com>
2026-08-12 18:08:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Cache-first service worker. Everything is local, so offline is the normal case. */
-const CACHE = "paper-investor-v5";
+const CACHE = "paper-investor-v6";
 const ASSETS = [
   "./",
   "./index.html",
